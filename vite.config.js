18d428ded00c5ebdd4af9bd21327b5c4585e8342
Rbr4t <robert.koor@opilased.nrg.edu.ee>
2023-04-28 20:22:21 +0300

    pages not working, trying a fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [svelte()],
   build: {
     outDir: './docs', // relative to index.html
-    // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
+    emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
   }
 })
