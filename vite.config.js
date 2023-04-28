e567d10cbb222d42097f0dedc246029ba23c84aa
Rbr4t <robert.koor@opilased.nrg.edu.ee>
2023-04-28 20:28:37 +0300

    something's still wrong

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,5 @@ export default defineConfig({
   plugins: [svelte()],
   build: {
     outDir: './docs', // relative to index.html
-    emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
   }
 })
